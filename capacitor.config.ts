990fb06cb377963d6cd4c25ee55a612592ab7f5e
marianorluna <marianorluna@gmail.com>
2026-03-15 22:40:50 +0100

    fix: add capacitor and modify gitignore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arqfi.stockearly',
+  appName: 'StockEarly',
+  webDir: 'dist'
+};
+
+export default config;
